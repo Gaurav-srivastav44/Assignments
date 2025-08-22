bc2c480a15c534ec2270046776655bc28c761481
Gaurav-srivastav44 <gaurav.srivastav_cs23@gla.ac.in>
2025-08-22 23:29:34 +0530

    A55.1: Initial commit

---
+++ src/tableRow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TableRow({ number, multiplier }) {
+  return (
+    <p className="text-2xl text-blue-700 my-2">
+      {number} x {multiplier} = {number * multiplier}
+    </p>
+  );
+}
+
+export default TableRow;
